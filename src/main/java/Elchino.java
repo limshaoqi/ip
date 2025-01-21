eb73801c7db819f13fc98b06beb0d44c00a754ec
Lim Shao Qi <>
2025-01-21 19:07:51 +0800

    renamed Duke to El Chino and added custom messages

---
+++ src/main/java/Elchino.java
@@ -0,0 +1,9 @@
+public class Elchino {
+    private static final String name = "El Chino";
+
+    public static void main(String[] args) {
+        System.out.println("Hola! Yo me llamo " + name + ". Encantado.");
+        System.out.println("Hasta pronto!");
+    }
+}
+
