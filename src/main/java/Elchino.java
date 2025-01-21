74e92a0df094684d95f96b00c5001f86a7d5eea0
Lim Shao Qi <>
2025-01-21 20:53:25 +0800

    added echo user input and exit when 'bye'

--- src/main/java/Elchino.java
+++ src/main/java/Elchino.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Elchino {
     private static final String name = "El Chino";
 
     public static void main(String[] args) {
         System.out.println("Hola! Yo me llamo " + name + ". Encantado.");
-        System.out.println("Hasta pronto!");
+        System.out.println("¿Le puedo ayudar en algo?");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String token = scanner.nextLine();
+            if (token.equals("bye")) {
+                System.out.println("Hasta pronto!");
+                return;
+            }
+            System.out.println(token);
+        }
     }
 }
 
