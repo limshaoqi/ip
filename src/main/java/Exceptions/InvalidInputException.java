a79fe2d11ab1ca93b98363e029db3e1af442db77
Lim Shao Qi <>
2025-01-24 18:11:55 +0800

    Add InvalidInputException

---
+++ src/main/java/Exceptions/InvalidInputException.java
@@ -0,0 +1,9 @@
+public class InvalidInputException extends ElchinoException {
+    public InvalidInputException() {
+        super("Entrada inválida: Por favor ingresa un número válido.");
+    }
+
+    public InvalidInputException(int taskNumber) {
+        super("Lo siento, no puedo encontrar la tarea " + taskNumber + ".");
+    }
+}
