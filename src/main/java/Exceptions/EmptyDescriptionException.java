6d1a5af146010f80ed6a27a344fc357f3f417383
Lim Shao Qi <>
2025-01-24 18:26:29 +0800

    Add EmptyDescriptionException

---
+++ src/main/java/Exceptions/EmptyDescriptionException.java
@@ -0,0 +1,5 @@
+public class EmptyDescriptionException extends ElchinoException {
+    public EmptyDescriptionException() {
+        super("Descripción vacía: Por favor ingresa una descripción válida.");
+    }
+}
