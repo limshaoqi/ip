9b0646939b59cae336993749fdf4f549c1cecdb4
Lim Shao Qi <>
2025-01-29 22:06:59 +0800

    Add Storage.java to manage saving tasks

---
+++ src/main/java/Storage.java
@@ -0,0 +1,10 @@
+public class Storage {
+    private static final String FILE_PATH;
+        /* Create and Write to files inspired by:
+        https://www.w3schools.com/java/java_files_create.asp
+         */
+    public Storage(String filePath) {
+        FILE_PATH = filePath;
+    }
+        
+}
