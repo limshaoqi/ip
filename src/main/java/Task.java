7670e976c5e73a4e8f420770d4fd9183b1f6a3ca
Lim Shao Qi <>
2025-01-31 18:10:15 +0800

    Add date time parser

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,11 @@
+import java.time.format.DateTimeFormatter;
+
 public abstract class Task {
     protected final String description;
     protected boolean isDone;
+
+    protected static final DateTimeFormatter INPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    protected static final DateTimeFormatter OUTPUT_DATE_FORMAT = DateTimeFormatter.ofPattern("dd MMM yyyy, h:mma");
 
     public Task(String description) {
         this.description = description;
@@ -26,5 +31,13 @@ public abstract class Task {
     @Override
     public String toString() {
         return String.format("[%s] %s", isDone ? "X" : " ", description);
+    }
+
+    protected static LocalDateTime parseDateTime(String dateTime) {
+        try {
+            return LocalDateTime.parse(dateTime, INPUT_DATE_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Formato de fecha inválido. Por favor, use el formato dd/MM/yyyy HHmm.");
+        }
     }
 }
